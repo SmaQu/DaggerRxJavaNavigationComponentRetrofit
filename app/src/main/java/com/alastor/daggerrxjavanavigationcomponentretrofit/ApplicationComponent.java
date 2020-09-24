a632677b89b3d14c93680da79378daaba34ca572
Maciek <maciej.smolik95@gmail.com>
2020-09-24 21:01:31 +0200

    Add ApplicationComponent

---
+++ app/src/main/java/com/alastor/daggerrxjavanavigationcomponentretrofit/ApplicationComponent.java
@@ -0,0 +1,25 @@
+package com.alastor.daggerrxjavanavigationcomponentretrofit;
+
+import android.app.Application;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjectionModule;
+import dagger.android.AndroidInjector;
+
+@Component(
+        modules = {
+                AndroidInjectionModule.class
+        }
+)
+public interface ApplicationComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        Builder application(Application application);
+
+        ApplicationComponent build();
+    }
+}
---
+++ app/src/main/java/com/alastor/daggerrxjavanavigationcomponentretrofit/BaseApplication.java
@@ -0,0 +1,12 @@
+package com.alastor.daggerrxjavanavigationcomponentretrofit;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DaggerApplication;
+
+public class BaseApplication extends DaggerApplication {
+
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerApplicationComponent.builder().application(this).build();
+    }
+}
